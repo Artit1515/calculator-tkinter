7f03e8e2fd2098e5e711e2ec9deb47d6d74bca3b
Artitttt <98724107+Artit1515@users.noreply.github.com>
2022-07-03 12:31:22 +0000

    แก้ logic

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import math
 app=Tk()
 app.title("calculator")
 #app.geometry("600x600")
-
+app.config(bg="#374353")
 
 ansresult=""
 
@@ -23,24 +23,91 @@ def clear():
 
 def equal():
     sub_str ="/0"
+    sub_str2=")("
+    countdot=0
+    countmutiply=0
+    countdivide=0
+    countopblacket=0
+    countclblacket=0
     global ansresult
     firstchar=ansresult[0]
+    lastchar=ansresult[-1]
+    for A in ansresult:
+        if A ==".":
+            countdot+=1
+
+    if countdot >1:
+        ansresult=""
+        dpshow.set("syntrax error")
+        return
+###########################################       
+    for B in ansresult:
+        if B =="*":
+            countmutiply+=1
+
+    if countmutiply >1:
+        ansresult=""
+        dpshow.set("syntrax error")
+        return
+#######################################
+    for E in ansresult:
+        if E =="/":
+            countdivide+=1
+
+    if countdivide >1:
+        ansresult=""
+        dpshow.set("syntrax error")
+        return
+#######################################
+    for C in ansresult:
+        if C =="(":
+            countopblacket+=1
+
+    for D in ansresult:
+        if D ==")":
+            countmutiply+=1
+
+    if countopblacket >0:
+        if countopblacket != countclblacket:
+            ansresult=""
+            dpshow.set("syntrax error")
+            return
+#######################################
     if firstchar == "*" or firstchar == "/" or firstchar == ")":
         ansresult=""
         dpshow.set("syntrax error")
         return
 
-    if sub_str in ansresult:
+
+    #if firstchar == "+" or firstchar == "-":
+    #    ansresult=""
+     #   dpshow.set("0")
+     #   return
+
+    if lastchar == "+" or lastchar == "-" or lastchar == "*" or lastchar == "/" or lastchar == "(":
         ansresult=""
         dpshow.set("syntrax error")
         return
 
+    if sub_str in ansresult or sub_str2 in ansresult :
+        ansresult=""
+        dpshow.set("syntrax error")
+        return
+
+    if ansresult=="()":
+        ansresult=""
+        dpshow.set("0")
+        return
 
 
     finalans=float(eval(ansresult))
     dpshow.set(str(finalans))
     
     ansresult=str(finalans)
+    with open("history calsutation.txt", "a") as f:
+        writehistory=ansresult+"\n"
+        f.write(writehistory)
+        f.close()
 
 def delete():
     global ansresult
@@ -52,7 +119,10 @@ def delete():
     if lenght == 1:
         dpshow.set(0)
         ansresult=""
-
+    if ans == "syntrax error":
+        dp.delete(0,END)
+        dpshow.set(0)
+        ansresult=""
 
 def root():
     global ansresult
@@ -89,21 +159,21 @@ myMenu.add_cascade(label="more",menu=minimenu)
 
 
 #ที่เก็บเลข
-bt7=Button(text="7",padx=30,bg="light gray",pady=30,command=lambda:enter(7)).grid(row=2,column=0)
-bt8=Button(text="8",padx=30,bg="light gray",pady=30,command=lambda:enter(8)).grid(row=2,column=1)
-bt9=Button(text="9",padx=30,bg="light gray",pady=30,command=lambda:enter(9)).grid(row=2,column=2)
+bt7=Button(text="7",padx=30,bg="#393030",fg="white",pady=30,command=lambda:enter(7)).grid(row=2,column=0)
+bt8=Button(text="8",padx=30,bg="#393030",fg="white",pady=30,command=lambda:enter(8)).grid(row=2,column=1)
+bt9=Button(text="9",padx=30,bg="#393030",fg="white",pady=30,command=lambda:enter(9)).grid(row=2,column=2)
 
-bt4=Button(text="4",padx=30,bg="light gray",pady=30,command=lambda:enter(4)).grid(row=3,column=0)
-bt5=Button(text="5",padx=30,bg="light gray",pady=30,command=lambda:enter(5)).grid(row=3,column=1)
-bt6=Button(text="6",padx=30,bg="light gray",pady=30,command=lambda:enter(6)).grid(row=3,column=2)
+bt4=Button(text="4",padx=30,bg="#393030",fg="white",pady=30,command=lambda:enter(4)).grid(row=3,column=0)
+bt5=Button(text="5",padx=30,bg="#393030",fg="white",pady=30,command=lambda:enter(5)).grid(row=3,column=1)
+bt6=Button(text="6",padx=30,bg="#393030",fg="white",pady=30,command=lambda:enter(6)).grid(row=3,column=2)
 
-bt1=Button(text="1",padx=30,bg="light gray",pady=30,command=lambda:enter(1)).grid(row=4,column=0)
-bt2=Button(text="2",padx=30,bg="light gray",pady=30,command=lambda:enter(2)).grid(row=4,column=1)
-bt3=Button(text="3",padx=30,bg="light gray",pady=30,command=lambda:enter(3)).grid(row=4,column=2)
+bt1=Button(text="1",padx=30,bg="#393030",fg="white",pady=30,command=lambda:enter(1)).grid(row=4,column=0)
+bt2=Button(text="2",padx=30,bg="#393030",fg="white",pady=30,command=lambda:enter(2)).grid(row=4,column=1)
+bt3=Button(text="3",padx=30,bg="#393030",fg="white",pady=30,command=lambda:enter(3)).grid(row=4,column=2)
 
-bt0=Button(text="0",padx=30,pady=30,bg="light gray",command=lambda:enter(0)).grid(row=5,column=0)
-btdot=Button(text=".",padx=31.25,bg="light gray",pady=30,command=lambda:enter(".")).grid(row=5,column=1)
-btequal=Button(text="=",padx=30,bg="light gray",pady=30,command=equal).grid(row=5,column=2)
+bt0=Button(text="0",padx=30,pady=30,bg="#393030",fg="white",command=lambda:enter(0)).grid(row=5,column=1)
+btdot=Button(text=".",padx=31.25,bg="#393030",fg="white",pady=30,command=lambda:enter(".")).grid(row=5,column=0)
+btequal=Button(text="=",padx=30,bg="#015DB2",pady=30,command=equal).grid(row=5,column=2)
 
 btplus=Button(text="+",padx=30,bg="light gray",pady=30,command=lambda:enter("+")).grid(row=5,column=3)
 btminus=Button(text="-",padx=31.75,bg="light gray",pady=30,command=lambda:enter("-")).grid(row=4,column=3)
@@ -111,8 +181,8 @@ btmultiply=Button(text="x",padx=31,bg="light gray",pady=30,command=lambda:enter(
 btdivide=Button(text="÷",padx=30,bg="light gray",pady=30,command=lambda:enter("/")).grid(row=2,column=3)
 
 
-btdelelte=Button(text="del",padx=26.5,bg="light gray",pady=30,command=delete).grid(row=1,column=3)
-dtclear=Button(text="c",padx=30,bg="light gray",pady=30,command=clear).grid(row=1,column=2)
+btdelelte=Button(text="del",padx=26.5,bg="light gray",fg="red",pady=30,command=delete).grid(row=1,column=3)
+dtclear=Button(text="c",padx=30,bg="light gray",fg="red",pady=30,command=clear).grid(row=1,column=2)
 btopblacket=Button(text=")",padx=30.5,bg="light gray",pady=30,command=lambda:enter(")")).grid(row=1,column=1)
 btclblacket=Button(text="(",padx=30.5,bg="light gray",pady=30,command=lambda:enter("(")).grid(row=1,column=0)
 
